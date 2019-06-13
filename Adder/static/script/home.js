02efea51ac835b9d35c836d18916ac55dab0ce2e
Prashant Singh <prashantxc8@gmail.com>
2019-06-13 16:52:33 +0530

    Basic functionality wit jQuery

--- Adder/static/script/home.js
+++ Adder/static/script/home.js
@@ -2,9 +2,29 @@ window.onload = function() {
 
     $(function(){
 
+        // NavBar
         $('.navbar-toggler').click(function(event){
             $('.navbar-collapse').toggle(500);
         });
+
+        $('#contact').on('click', function(event){
+            alert('Contact us on +9111 25635 698');
+        });
+
+        // Selector
+        $('.dropdown-toggle').on('click', function(event){
+            $('.dropdown-menu').toggle(100);
+        });
+
+        $('.dropdown-item').on('click', function(event){
+            var selectedData = $(this).html();
+            $('.dropdown-toggle').html(selectedData);
+            $('.dropdown-menu').toggle(100);
+
+            showList(selectedData);
+        });
+
+        
 
     });
 
